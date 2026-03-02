e877e268d8655770ed14c9845bfdcf6013baaa1c
mdabdullahcse11 <md.abdullah.cse11@gmail.com>
2026-03-03 05:36:31 +0600

    working with After clicking every tab hidden and visual

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 let currentTab = 'all';
 const tabActive = [ "bg-black", "border-black", "text-white"];
-const tabInactive = ["bg-transparent", "text-slate-700", "border-state-200", ]
+const tabInactive = ["bg-transparent", "text-slate-700", "border-state-200","text-black", ]
+
+const allContainer = document.getElementById ("all-container");
+
+const interviewContainer = document.getElementById ("interview-container");
+
+const rejectContainer = document.getElementById ("reject-container");
+
 
 
 function switchTab (tab){
-    console.log(tab)
+ 
+
     const tabs = [ "all","interview", "rejected" ]
     for (const t of tabs ) {
         const tabName = document.getElementById("tab-"+ t )
     if (t === tab){
+
   tabName.classList.remove(...tabInactive);
   tabName.classList.add(...tabActive);
     }
@@ -18,4 +27,24 @@ function switchTab (tab){
         tabName.classList.add(...tabInactive);
     }
     }
+    const pages = [allContainer,interviewContainer,rejectContainer];
+
+    for (const section of pages ){
+        section.classList.add("hidden")
+    }
+
+
+    if(tab === "all"){
+        allContainer.classList.remove("hidden");  
+    }
+    else if ( tab === "interview" ){
+        interviewContainer.classList.remove("hidden")
+    }
+
+    else {
+        rejectContainer.classList.remove ("hidden")
+    }
+
+    
 } 
+switchTab (currentTab);
